1e0acff9a39010d26c39f7948db7267d1c9f92aa
avinash <avinash@ghumloo.com>
2025-07-03 17:19:41 +0530

    update in code

--- resources/views/dash/StatusOfExpenses/preview.blade.php
+++ resources/views/dash/StatusOfExpenses/preview.blade.php
@@ -58,7 +58,7 @@
                     <div class="card-dialog card-dialog-centered card-fullscreen">
 
                         <div class="card-header" style="background-color:#077fad; color:white;">
-                          <h4 class="" style="height: 50px;">Preview of {{ \Carbon\Carbon::parse($month)->format('F') }} Expenses
+                          <h4 class="" style="height: 50px;"> {{ \Carbon\Carbon::parse($month)->format('F') }} Expense  Report
                             <button id="download-pdf" class="btn btn-primary exclude-from-pdf">Download PDF</button>
                           </h4>
                         </div>
